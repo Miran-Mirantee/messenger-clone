0e1ce4be39c2fa4dc6532716624cc425040f8312
Miran-Mirantee <killerm11805@hotmail.com>
2023-08-11 21:14:32 +0700

    display other users, create new conversation api

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,12 +1,17 @@
 import Sidebar from "../components/sidebar/Sidebar";
+import UserList from "./components/UserList";
+import getUsers from "../actions/getUsers";
 
 export default async function UsersLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const users = await getUsers();
+
   return (
     <Sidebar>
+      <UserList items={users} />
       <div className="h-full">{children}</div>
     </Sidebar>
   );
