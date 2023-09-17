54a1784edb572c2d1ebfa75a2ed7e8adb201d51f
Miran-Mirantee <killerm11805@hotmail.com>
2023-09-18 02:42:12 +0700

    unit test WIP

---
+++ mocks/handlers.ts
@@ -0,0 +1,24 @@
+import { rest } from "msw";
+
+export const handlers = [
+  rest.get("http://www.boredapi.com/api/activity/", (req, res, ctx) => {
+    return res(ctx.status(200));
+  }),
+];
+
+// export const handlers = [
+//   rest.get("http://www.boredapi.com/api/activity/", (req, res, ctx) => {
+//     return res(
+//       ctx.status(200),
+//       ctx.json({
+//         activity: "Learn Express.js",
+//         accessibility: 0.25,
+//         type: "education",
+//         participants: 1,
+//         price: 0.1,
+//         link: "https://expressjs.com/",
+//         key: "3943506",
+//       })
+//     );
+//   }),
+// ];
