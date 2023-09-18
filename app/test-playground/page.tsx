baaf79dee5af701763f94482e317b7974ae70fd7
Miran-Mirantee <82795807+Miran-Mirantee@users.noreply.github.com>
2023-09-18 17:43:50 +0700

    fix unable to fetch

--- app/test-playground/page.tsx
+++ app/test-playground/page.tsx
@@ -46,117 +46,125 @@ const TestPlayground = () => {
 
   const [activity, setActivity] = useState<Activity | null>();
   return (
-    <div className="flex p-4 flex-col items-center">
-      <form
-        onSubmit={handleSubmit(onsubmit)}
-        className="w-[480px] flex flex-col gap-6"
-      >
-        <Card size={"md"} className="h-[224px]">
-          <CardBody className="flex flex-col justify-center">
-            {activity?.error ? (
-              <div>
-                <span className="font-bold">Error: </span>
-                {activity?.error}
-              </div>
-            ) : (
-              <>
+    <>
+      <head>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </head>
+      <div className="flex p-4 flex-col items-center">
+        <form
+          onSubmit={handleSubmit(onsubmit)}
+          className="w-[480px] flex flex-col gap-6"
+        >
+          <Card size={"md"} className="h-[224px]">
+            <CardBody className="flex flex-col justify-center">
+              {activity?.error ? (
                 <div>
-                  <span className="font-bold">Activity: </span>
-                  {activity?.activity}
+                  <span className="font-bold">Error: </span>
+                  {activity?.error}
                 </div>
-                <div>
-                  <span className="font-bold">Type: </span> {activity?.type}
-                </div>
-                <div>
-                  <span className="font-bold">Number of participants: </span>
-                  {activity?.participants}
-                </div>
-                <div>
-                  <span className="font-bold">Price: </span>
-                  {activity?.price}
-                </div>
-                <div>
-                  <span className="font-bold">Accessibility: </span>
-                  {activity?.accessibility}
-                </div>
-                <div>
-                  <span className="font-bold">Link: </span>
-                  {activity?.link}
-                </div>
-              </>
-            )}
-          </CardBody>
-        </Card>
-        <FormControl>
-          <FormLabel>Type of activity</FormLabel>
-          <Select placeholder="Select type" {...register("type")}>
-            <option value="education">education</option>
-            <option value="recreational">recreational</option>
-            <option value="social">social</option>
-            <option value="diy">diy</option>
-            <option value="charity">charity</option>
-            <option value="cooking">cooking</option>
-            <option value="relaxation">relaxation</option>
-            <option value="music">music</option>
-            <option value="busywork">busywork</option>
-          </Select>
-        </FormControl>
-        <FormControl>
-          <FormLabel>Number of participant</FormLabel>
-          <NumberInput min={1}>
-            <NumberInputField {...register("participants")} />
-            <NumberInputStepper>
-              <NumberIncrementStepper />
-              <NumberDecrementStepper />
-            </NumberInputStepper>
-          </NumberInput>
-        </FormControl>
-        <RadioGroup>
-          <FormLabel>Price range</FormLabel>
+              ) : (
+                <>
+                  <div>
+                    <span className="font-bold">Activity: </span>
+                    {activity?.activity}
+                  </div>
+                  <div>
+                    <span className="font-bold">Type: </span> {activity?.type}
+                  </div>
+                  <div>
+                    <span className="font-bold">Number of participants: </span>
+                    {activity?.participants}
+                  </div>
+                  <div>
+                    <span className="font-bold">Price: </span>
+                    {activity?.price}
+                  </div>
+                  <div>
+                    <span className="font-bold">Accessibility: </span>
+                    {activity?.accessibility}
+                  </div>
+                  <div>
+                    <span className="font-bold">Link: </span>
+                    {activity?.link}
+                  </div>
+                </>
+              )}
+            </CardBody>
+          </Card>
+          <FormControl>
+            <FormLabel>Type of activity</FormLabel>
+            <Select placeholder="Select type" {...register("type")}>
+              <option value="education">education</option>
+              <option value="recreational">recreational</option>
+              <option value="social">social</option>
+              <option value="diy">diy</option>
+              <option value="charity">charity</option>
+              <option value="cooking">cooking</option>
+              <option value="relaxation">relaxation</option>
+              <option value="music">music</option>
+              <option value="busywork">busywork</option>
+            </Select>
+          </FormControl>
+          <FormControl>
+            <FormLabel>Number of participant</FormLabel>
+            <NumberInput min={1}>
+              <NumberInputField {...register("participants")} />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+          </FormControl>
+          <RadioGroup>
+            <FormLabel>Price range</FormLabel>
+            <HStack>
+              <Radio value="1" {...register("priceRange")}>
+                Free
+              </Radio>
+              <Radio value="2" {...register("priceRange")}>
+                Cheap
+              </Radio>
+              <Radio value="3" {...register("priceRange")}>
+                Costly
+              </Radio>
+            </HStack>
+          </RadioGroup>
+          <RadioGroup>
+            <FormLabel>Accesibility range</FormLabel>
+            <HStack>
+              <Radio value="1" {...register("accessibilityRange")}>
+                Easy
+              </Radio>
+              <Radio value="2" {...register("accessibilityRange")}>
+                Normal
+              </Radio>
+              <Radio value="3" {...register("accessibilityRange")}>
+                Hard
+              </Radio>
+            </HStack>
+          </RadioGroup>
           <HStack>
-            <Radio value="1" {...register("priceRange")}>
-              Free
-            </Radio>
-            <Radio value="2" {...register("priceRange")}>
-              Cheap
-            </Radio>
-            <Radio value="3" {...register("priceRange")}>
-              Costly
-            </Radio>
+            <Button
+              onClick={async () => {
+                const fetchedData = await getActivity();
+                console.log(fetchedData);
+                setActivity(fetchedData);
+              }}
+              colorScheme="teal"
+              variant="outline"
+            >
+              Random
+            </Button>
+            <Button type="submit" colorScheme="teal" className="bg-[#319795ec]">
+              Submit
+            </Button>
           </HStack>
-        </RadioGroup>
-        <RadioGroup>
-          <FormLabel>Accesibility range</FormLabel>
-          <HStack>
-            <Radio value="1" {...register("accessibilityRange")}>
-              Easy
-            </Radio>
-            <Radio value="2" {...register("accessibilityRange")}>
-              Normal
-            </Radio>
-            <Radio value="3" {...register("accessibilityRange")}>
-              Hard
-            </Radio>
-          </HStack>
-        </RadioGroup>
-        <HStack>
-          <Button
-            onClick={async () => {
-              const fetchedData = await getActivity();
-              console.log(fetchedData);
-              setActivity(fetchedData);
-            }}
-            colorScheme="teal"
-            variant="outline"
-          >
-            Random
-          </Button>
-          <Button type="submit" colorScheme="teal" className="bg-[#319795ec]">
-            Submit
-          </Button>
-        </HStack>
-      </form>
-    </div>
+        </form>
+      </div>
+    </>
   );
 };
 
