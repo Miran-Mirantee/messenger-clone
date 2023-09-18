1812ca7af202bce3fe9e5a1767654d6f7887bd95
Miran-Mirantee <82795807+Miran-Mirantee@users.noreply.github.com>
2023-09-18 17:39:30 +0700

    allow unsafe_url fetch

--- app/actions/getActivity.ts
+++ app/actions/getActivity.ts
@@ -17,7 +17,8 @@ export default async function getActivity({
 }: Inputs = {}): Promise<any> {
   try {
     const response = await fetch(
-      `http://www.boredapi.com/api/activity/?type=${type}&participants=${participants}&minprice=${minPrice}&maxprice=${maxPrice}&minaccessibility=${minAccessibility}&maxaccessibility=${maxAccessibility}`
+      `http://www.boredapi.com/api/activity/?type=${type}&participants=${participants}&minprice=${minPrice}&maxprice=${maxPrice}&minaccessibility=${minAccessibility}&maxaccessibility=${maxAccessibility}`,
+      { referrerPolicy: "unsafe-url" }
     );
     const data = await response.json();
     return data;
